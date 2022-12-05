7b9385867967bf0134b54acb82389e52b0d03aae
brookeses69 <83194919+brookeses69@users.noreply.github.com>
2022-12-05 18:03:01 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,14 @@
 import os
+import re
 
-list1=["kube-apiserver","kube-controller-manager","kube-scheduler","kube-proxy","pause","etcd","coredns"
-]
-for item in list1:#直接遍历
-    path = item
-    print(path)
-    os.mkdir(path)
+a = ["kube-apiserver:v1.25.4",
+"kube-controller-manager:v1.25.4",
+"kube-scheduler:v1.25.4",
+"kube-proxy:v1.25.4",
+"pause:3.8",
+"etcd:3.5.4-0",
+"coredns:v1.9.3",]  # 二级目录需要删除一个级别
+for item in a:#直接遍历
+    lst = re.findall(r".*:", item)
+    lst = lst[0].replace(':', '')
+    os.mkdir(lst)
